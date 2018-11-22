51df4364d59f526f3cac9912329a02b34978bae4
Yuriy Sorokin <yurich00@gmail.com>
2018-11-22 23:20:40 +0600

    trytravis-2018-11-22T23:20:40.301155

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -64,3 +64,16 @@ resource "google_compute_firewall" "firewall_puma" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["reddit-app"]
 }
+
+resource "google_compute_firewall" "firewall_puma_80" {
+  name    = "allow-puma-80-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
